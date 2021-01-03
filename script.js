bd94c6955e326b44093771147f1ad2280531b910
richardvo299 <68835147+richardvo299@users.noreply.github.com>
2021-01-04 03:31:19 +0700

    Add files via upload
    Almost-last styling editing

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const API_KEY_JULIEN = "db84944e4adc4398803af0162c647905";
+const API_KEY_JULIEN = "defd19c83e5844bd88287d0bba078e59";
+let newsState = [];
+let filteredNewsState = [];
+let sourcesToFilter = [];
+let sourcesFiltered = [];
+let pageNumber = 1;
 
 const drawCard = (news) => {
-  return `<div class="example-2 card">
-<div class="wrapper" style="background: url(${news.urlToImage}) 20% 1%/cover no-repeat;">
+  return `<br><div class="example-2 card">
+<div class="wrapper" style="background: url(${
+    news.urlToImage
+  }) 20% 1%/cover no-repeat;">
+
 <div class="header">
   <div class="date">
     <div href="#" class="card-link">${moment(news.publishedAt).fromNow()}</div>
@@ -18,12 +26,71 @@ const drawCard = (news) => {
 </div>
 </div>
 </div>`;
-            
+
 };
 
-const removeNews = () => {
+const drawFilterBar = (news) => {
+  const sources = newsState
+    .map((article) => article)
+    .reduce((sources, article) => {
+      sources[`${article.source.name}`] = news.filter(
+        (test) => test.source.name === article.source.name
+      ).length;
+      return sources;
+      //sources = {Source name 1 : Number of stories, Source name 2 : Number of stories...}
+    }, {});
+  let output = [];
+
+  // Builds HTML
+  // If Source is filtered out (in sourcestoFilter), uncheck
+  Object.entries(sources).forEach((source) => {
+    output.push(`<div>
+    <label for="">${source[0]} (${
+      source[1]
+    })</label><input type="checkbox"  id="${source[0]}" ${
+      sourcesToFilter.includes(source[0]) ? "" : "checked"
+    }/>
+    </div>`);
+  });
+
+  sourcesFiltered = output.join("\n");
+  return output.join("\n");
+};
+
+// Add Change event listener to all checkboxes
+// If checked -> push source in sourcesToFilter
+// Else -> Remove source from sourcesToFilter
+// Call filternews
+const createEventListenersFilter = () => {
+  const checkboxes = document.querySelectorAll("input[type = checkbox]");
+  checkboxes.forEach((checkbox) =>
+    checkbox.addEventListener("change", (e) => {
+      if (e.target.outerHTML.includes("checked")) {
+        sourcesToFilter.push(e.target.id);
+      } else {
+        sourcesToFilter.splice(sourcesToFilter.indexOf(e.target.id, 1));
+      }
+      filterNews();
+    })
+  );
+};
+
+// Filters  blacklisted sources out of newsState
+// Calls render
+const filterNews = () => {
+  filteredNewsState = newsState.filter(
+    (news) => !sourcesToFilter.includes(news.source.name)
+  );
+  render(filteredNewsState);
+};
+
+const clearNews = () => {
   const newsSection = document.getElementById("news");
   newsSection.innerHTML = "";
+};
+const clearFilters = () => {
+  const filterSection = document.getElementById("filter-div");
+  filterSection.innerHTML = "";
 };
 const searchBar = document
   .getElementById("searchbar")
@@ -33,21 +100,58 @@ const searchBar = document
 const update = async (requestType) => {
   const url = requestType
     ? `https://newsapi.org/v2/top-headlines?q=${requestType}&country=us&apiKey=db84944e4adc4398803af0162c647905`
-    : "https://newsapi.org/v2/top-headlines?country=us&apiKey=db84944e4adc4398803af0162c647905";
+    : `https://newsapi.org/v2/top-headlines?country=us&page=${pageNumber}&apiKey=db84944e4adc4398803af0162c647905`;
   const res = await fetch(url);
   const data = await res.json();
-  render(data.articles);
+  newsState = data.articles;
+  render(newsState);
 };
 
 const render = (news) => {
-  removeNews();
+  clearNews();
+  clearFilters();
   const newsSection = document.getElementById("news");
+  const aboveNews = document.getElementById("left-side-menu");
   const outputHtml = document.createElement("div");
+  const filterHtml = document.getElementById("filter-div");
   const output = news.map((article) => {
     return drawCard(article);
   });
   outputHtml.innerHTML = output.join("\n");
+  newsSection.innerHTML = `<div>Number of results : ${news.length}</div>`;
   newsSection.appendChild(outputHtml);
+  filterHtml.innerHTML = drawFilterBar(news);
+
+  createEventListenersFilter();
+  aboveNews.appendChild(filterHtml);
 };
 
+document.getElementById("business").addEventListener("click", () => {
+  update("business");
+});
+document.getElementById("entertainment").addEventListener("click", () => {
+  update("entertainment");
+});
+document.getElementById("technology").addEventListener("click", () => {
+  update("tech");
+});
+document.getElementById("sports").addEventListener("click", () => {
+  update("game");
+});
+document.getElementById("health").addEventListener("click", () => {
+  update("health");
+});
+
+document.getElementById("next").addEventListener("click", (e) => {
+  pageNumber++;
+  update();
+});
+document.getElementById("previous").addEventListener("click", (e) => {
+  pageNumber--;
+  update();
+});
+
+document.getElementById("hamburger").addEventListener("click", () => {
+  document.getElementById("sidebar-wrapper").classList.toggle("invisible");
+});
 update();
